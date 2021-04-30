fe5722c6874fd8de39d83b0c9ccea14e55be2527
王鹏飞 <wangpengfei@bond520.com>
2021-04-30 14:34:39 +0800

    * 代码格式调整.

--- src/components/api/API.tsx
+++ src/components/api/API.tsx
@@ -1,34 +1,6 @@
 import React from 'react'
-import { serve } from '../../relatives/services/constant'
 import './API.css'
-import { Paper, Button } from '@material-ui/core'
-
-const ExampleJQuery = () => (
-  <div>
-    <ul>
-      <li>先引入jQuery插件</li>
-      <li>再引入基础插件</li>
-      <li>最后引入RAP jQuery插件</li>
-    </ul>
-    <h4>示例代码</h4>
-    <pre className="code-example">
-      {
-        '<script src="jquery.min.js"></script>\n' +
-        '<script src="http://rap2api.taobao.org/app/plugin/:projectId"></script>\n' +
-        '<script src="http://rap2api.taobao.org/libs/jquery.rap.js"></script>\n' +
-        '$.ajax({\n' +
-        '    url : \'/example/1501049256513\', // 自动拦截\n' +
-        '    method : \'GET\',\n' +
-        '    dataType : \'JSON\',\n' +
-        '    success : function(data) {\n' +
-        '      // 返回根据RAP文档及规则生成的mock数据\n' +
-        '      $(\'#result\').html(JSON.stringify(data))\n' +
-        '    }\n' +
-        '})\n'
-      }
-    </pre>
-  </div>
-)
+import { Paper } from '@material-ui/core'
 
 // DONE 2.3 区分请求和响应作用域
 
